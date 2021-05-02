26b70532e598ebaa103253f0105a9894b8c52d83
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-02 17:24:39 -0400

    update image_urls for bubbleteas and remove description from table schema since not needed for MVP

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -24,8 +24,7 @@ CREATE TABLE users (
 CREATE TABLE bubbleteas (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
-  description TEXT,
-  cost INTEGER  NOT NULL DEFAULT 0,
+  cost INTEGER NOT NULL DEFAULT 0,  -- Cost in cents
   image_url VARCHAR(255) NOT NULL
 );
 
--- db/seeds/02_bubbleteas.sql
+++ db/seeds/02_bubbleteas.sql
@@ -1,14 +1,14 @@
-INSERT INTO bubbleteas (name, description, cost, image_url)
+INSERT INTO bubbleteas (name, cost, image_url)
 VALUES
-('Brown Sugar Pearl Milk Tea', 'description', 4.99, 'image_url_here'),
-('Fresh Strawberry Milk Tea', 'description', 5.99, 'image_url_here'),
-('Matcha Cheese Milk Tea', 'description', 5.99, 'image_url_here'),
-('Mango Mix Milk Tea', 'description', 4.99, 'image_url_here'),
-('Jasmine Milk Tea Macchiato', 'description', 4.99, 'image_url_here'),
-('Brown Sugar Matcha Milk Tea', 'description', 5.99, 'image_url_here'),
-('Raspberry Milk Tea', 'description', 5.99, 'image_url_here'),
-('Red Bean Grass Jelly Milk Tea', 'description', 6.99, 'image_url_here'),
-('Passion Fruit Milk Tea', 'description', 5.99, 'image_url_here'),
-('Mount Fuji Snow Top Milk Tea', 'description', 6.99, 'image_url_here'),
-('Grass Jelly Pudding Milk Tea', 'description', 5.99, 'image_url_here')
-('Sakura Snow Matcha Latte', 'description', 7.99, 'image_url_here');
+('Brown Sugar Pearl Milk Tea', 499, '../images/1.jpg'),
+('Fresh Strawberry Milk Tea', 599, '../images/2.jpg'),
+('Matcha Cheese Milk Tea', 599, '../images/3.jpg'),
+('Mango Mix Milk Tea', 499, '../images/4.jpg'),
+('Jasmine Milk Tea Macchiato', 499, '../images/5.jpg'),
+('Brown Sugar Matcha Milk Tea', 599, '../images/6.jpg'),
+('Raspberry Milk Tea', 599, '../images/7.jpg'),
+('Red Bean Grass Jelly Milk Tea', 699, '../images/8.jpg'),
+('Passion Fruit Milk Tea', 599, '../images/9.jpg'),
+('Mount Fuji Snow Top Milk Tea', 699, '../images/10.jpg'),
+('Grass Jelly Pudding Milk Tea', 599, '../images/11.jpg'),
+('Sakura Snow Matcha Latte', 799, '../images/12.jpg');
