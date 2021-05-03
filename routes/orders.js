b7a41aa2d53bb66645d3f5acf66ad8ca3a68487d
Dylan <dcren2010@gmail.com>
2021-05-03 16:29:58 -0400

    update order query for quantity

--- routes/orders.js
+++ routes/orders.js
@@ -10,7 +10,8 @@ module.exports = (db) => {
   // Get all orders for a certain user (whether a customer or admin)
   router.get("/", (req, res) => {
     // const userID = req.session['user_id'];
-    const queryString = `SELECT orders.id as Order_ID, count(orders.id) * cost as total_price, orders.* FROM orders
+    const queryString = `SELECT orders.id as Order_ID, count(orders.id) * cost as total_price, count(quantity) quantity, orders.*
+                        FROM orders
                         JOIN users ON user_id = users.id
                         JOIN order_line_items ON orders.id = order_id
                         JOIN bubbleteas ON bubbletea_id = bubbleteas.id
