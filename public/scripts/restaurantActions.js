ba6be46467c33e087fd7f3c90bb8dd1da6845b29
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-04 19:41:01 -0400

    update some colours and create file for handling restaurant actions

---
+++ public/scripts/restaurantActions.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  console.log("working");
+  // $('#submitOrders').click(e => {
+  //   e.preventDefault();
+  //   // jQuery cannot handle deep array, so have to send string
+  //   const order = localStorage.getItem("bubbletea");
+  //   console.log(order);
+
+  //   $.ajax({
+  //     type: "POST",
+  //     url: "/orders",
+  //     data: order,
+  //     contentType: "application/json"   // This is to prevent have FormData
+  //   });
+
+  // });
+});
