480aed0b6da209a2c1114199bec8402b08beb6fc
Dylan <dcren2010@gmail.com>
2021-05-03 21:30:43 -0400

    update esline

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -11,44 +11,41 @@
 // });
 // })
 
-
-
-$(document).ready(function() {
-
- class StoreBubbletea {
-    static getBubbleteas(){
+$(document).ready(function () {
+  class StoreBubbletea {
+    static getBubbleteas() {
       let bubbleteas;
-      if(localStorage.getItem('bubbletea') === null){
-        bubbleteas=[];
+      if (localStorage.getItem("bubbletea") === null) {
+        bubbleteas = [];
       } else {
-        bubbleteas = JSON.parse(localStorage.getItem('bubbletea'));
+        bubbleteas = JSON.parse(localStorage.getItem("bubbletea"));
       }
       return bubbleteas;
     }
 
-    static addBubbletea(bubbletea){
+    static addBubbletea(bubbletea) {
       const bubbleteas = StoreBubbletea.getBubbleteas();
-      bubbleteas.push(bubbletea)
-      localStorage.setItem('bubbletea',JSON.stringify(bubbleteas));
+      bubbleteas.push(bubbletea);
+      localStorage.setItem("bubbletea", JSON.stringify(bubbleteas));
     }
 
-    static removeBubbletea(bubbletea){
-
-    }
-
+    static removeBubbletea(bubbletea) {}
   }
 
-// Add to Cart
-let imageUrl,bubbleteaName, bubbleteaPrice;
-let addToCartBtns = document.querySelectorAll('#addToCart');
-addToCartBtns.forEach(btn => btn.addEventListener('click', (e) => {
-  imageUrl = e.target.parentElement.parentElement.children[0].childNodes[1].currentSrc;
-  bubbleteaName = e.target.parentElement.children[0].innerHTML
-  bubbleteaPrice = e.target.parentElement.children[1].innerHTML
-  // console.log(imageUrl, bubbleteaName, bubbleteaPrice);
+  // Add to Cart
+  let imageUrl, bubbleteaName, bubbleteaPrice;
+  let addToCartBtns = document.querySelectorAll("#addToCart");
+  addToCartBtns.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      imageUrl =
+        e.target.parentElement.parentElement.children[0].childNodes[1]
+          .currentSrc;
+      bubbleteaName = e.target.parentElement.children[0].innerHTML;
+      bubbleteaPrice = e.target.parentElement.children[1].innerHTML;
+      // console.log(imageUrl, bubbleteaName, bubbleteaPrice);
 
-  const bubbletea = {imageUrl, bubbleteaName, bubbleteaPrice}
-  StoreBubbletea.addBubbletea(bubbletea);
-
-}));
+      const bubbletea = { imageUrl, bubbleteaName, bubbleteaPrice };
+      StoreBubbletea.addBubbletea(bubbletea);
+    })
+  );
 });
--- routes/login.js
+++ routes/login.js
@@ -3,20 +3,19 @@
  * Since this file is loaded in server.js into /login, these routes are mounted onto /login
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
-
   // FOR DEVELOPMENT PURPOSES: handle user login and set a cookie with the user id
   router.get("/:id", (req, res) => {
-    req.session['user_id'] = req.params.id;
-    res.redirect('/');
+    req.session["user_id"] = req.params.id;
+    res.redirect("/");
   });
 
   // Display log in form
   router.get("/", (req, res) => {
-    const userID = req.session['user_id'];
+    const userID = req.session["user_id"];
     const templateVars = { userID };
     res.render("login", templateVars);
   });
@@ -25,7 +24,7 @@ module.exports = (db) => {
   router.post("/", (req, res) => {
     // const { email, password } = req.body;
     // Are we supposed to query databse for user?
-    res.redirect('/login/1'); // Redirection for user 1
+    res.redirect("/login/1"); // Redirection for user 1
   });
 
   return router;
