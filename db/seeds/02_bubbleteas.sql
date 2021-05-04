0639b0499983580ad9bc0acb52b6b35ddaa56acd
Dylan <dcren2010@gmail.com>
2021-05-04 08:55:08 -0400

    cart item display & remove done

--- db/seeds/02_bubbleteas.sql
+++ db/seeds/02_bubbleteas.sql
@@ -1,14 +1,14 @@
 INSERT INTO bubbleteas (name, cost, image_url)
 VALUES
-('Brown Sugar Pearl Milk Tea', 499, '../images/1.jpg'),
-('Fresh Strawberry Milk Tea', 599, '../images/2.jpg'),
-('Matcha Cheese Milk Tea', 599, '../images/3.jpg'),
-('Mango Mix Milk Tea', 499, '../images/4.jpg'),
-('Jasmine Milk Tea Macchiato', 499, '../images/5.jpg'),
-('Brown Sugar Matcha Milk Tea', 599, '../images/6.jpg'),
-('Raspberry Milk Tea', 599, '../images/7.jpg'),
-('Red Bean Grass Jelly Milk Tea', 699, '../images/8.jpg'),
-('Passion Fruit Milk Tea', 599, '../images/9.jpg'),
-('Mount Fuji Snow Top Milk Tea', 699, '../images/10.jpg'),
-('Grass Jelly Pudding Milk Tea', 599, '../images/11.jpg'),
-('Sakura Snow Matcha Latte', 799, '../images/12.jpg');
+('Brown Sugar Pearl Milk Tea', 499, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/1.jpg?raw=true'),
+('Fresh Strawberry Milk Tea', 599, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/2.jpg?raw=true'),
+('Matcha Cheese Milk Tea', 599, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/3.jpg?raw=true'),
+('Mango Mix Milk Tea', 499, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/4.jpg?raw=true'),
+('Jasmine Milk Tea Macchiato', 499, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/5.jpg?raw=true'),
+('Brown Sugar Matcha Milk Tea', 599, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/6.jpg?raw=true'),
+('Raspberry Milk Tea', 599, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/7.jpg?raw=true'),
+('Red Bean Grass Jelly Milk Tea', 699, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/8.jpg?raw=true'),
+('Passion Fruit Milk Tea', 599, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/9.jpg?raw=true'),
+('Mount Fuji Snow Top Milk Tea', 699, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/10.jpg?raw=true'),
+('Grass Jelly Pudding Milk Tea', 599, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/11.jpg?raw=true'),
+('Sakura Snow Matcha Latte', 799, 'https://github.com/dylangit01/Bubbles-Delight/blob/master/public/images/12.jpg?raw=true');
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,11 +16,20 @@ $(document).ready(function () {
       localStorage.setItem("bubbletea", JSON.stringify(bubbleteas));
     }
 
-    static removeBubbletea(bubbletea) {}
+    static removeBubbletea(id) {
+      const bubbleteas = StoreBubbletea.getBubbleteas();
+      console.log(bubbleteas);
+      bubbleteas.forEach((bubbletea, index) => {
+        if (bubbletea.bubbleteaId == id) {
+          bubbleteas.splice(index, 1);
+        }
+      });
+      localStorage.setItem("bubbletea", JSON.stringify(bubbleteas));
+    }
   }
 
   // Add to Cart
-  let imageUrl, bubbleteaName, bubbleteaPrice;
+  let imageUrl, bubbleteaName, bubbleteaPrice, bubbleteaId;
   let addToCartBtns = document.querySelectorAll("#addToCart");
   addToCartBtns.forEach((btn) =>
     btn.addEventListener("click", (e) => {
@@ -28,10 +37,15 @@ $(document).ready(function () {
         e.target.parentElement.parentElement.children[0].childNodes[1]
           .currentSrc;
       bubbleteaName = e.target.parentElement.children[0].innerHTML;
-      bubbleteaPrice = e.target.parentElement.children[1].innerHTML;
-      // console.log(imageUrl, bubbleteaName, bubbleteaPrice);
-
-      const bubbletea = { imageUrl, bubbleteaName, bubbleteaPrice };
+      bubbleteaPrice = e.target.parentElement.children[1].children[0].innerHTML;
+      bubbleteaId = e.target.nextElementSibling.innerHTML;
+      // console.log(e.target.nextElementSibling.innerHTML);
+      const bubbletea = {
+        imageUrl,
+        bubbleteaName,
+        bubbleteaPrice,
+        bubbleteaId,
+      };
       StoreBubbletea.addBubbletea(bubbletea);
     })
   );
@@ -49,39 +63,47 @@ $(document).ready(function () {
   class cartUI {
     static displayCartItems() {
       const storedBubbleteas = StoreBubbletea.getBubbleteas();
-      let bubbleteas = [];
-      console.log(bubbleteas);
-      bubbleteas = storedBubbleteas;
+      const bubbleteas = storedBubbleteas;
       bubbleteas.forEach((bubbletea) => cartUI.addToCart(bubbletea));
     }
     static addToCart(bubbletea) {
-      const bubbleList = document.querySelector("#cart-items");
+      // const bubbleList = document.querySelector("#cart-items");
+      // const tableRow = document.createElement("tr");
+      // tableRow.innerHTML = `
+      //   <td class="align-middle p-1"><img src="${bubbletea.imageUrl}" style="width:120px; height:auto" alt=""/></td>
+      //   <td class="align-middle">${bubbletea.bubbleteaName}</td>
+      //   <td class="align-middle">${bubbletea.bubbleteaPrice}</td>
+      //   <td class="align-middle"><a href="#"><i class="bi bi-eraser fa-2x removeBubbletea" style="color:#ff0000"></i></a></td>
+      //   <td class="d-none">${bubbletea.bubbleteaId}</td>
+      // `;
+      // bubbleList.appendChild(tableRow);
 
-      const tableRow = document.createElement("tr");
-      tableRow.innerHTML = `
-        <td>${bubbletea.imageUrl}</td>
-        <td>${bubbletea.bubbleteaName}</td>
-        <td>${bubbletea.bubbleteaPrice}</td>
-        <td><a href="#" class="btn btn-outline-danger btn-sm delete">X</a></td>
-      `;
-      bubbleList.appendChild(tableRow);
+      $("<tr/>").html(`
+        <td class="align-middle p-1"><img src="${bubbletea.imageUrl}" style="width:120px; height:auto" alt=""/></td>
+        <td class="align-middle">${bubbletea.bubbleteaName}</td>
+        <td class="align-middle">${bubbletea.bubbleteaPrice}</td>
+        <td class="align-middle"><i class="bi bi-eraser fa-2x removeBubbletea" style="color:#ff0000"></i></td>
+        <td class="d-none">${bubbletea.bubbleteaId}</td>
+      `).appendTo('#cart-items')
+    }
 
-      // $("<tr/>").html(`
-      // <td>${bubbletea.imageUrl}</td>
-      // <td>${bubbletea.bubbleteaName}</td>
-      // <td>${bubbletea.bubbleteaPrice}</td>
-      // <td><a href="#" class="btn btn-outline-danger btn-sm delete">X</a></td>
-      // `).appendTo('#cart-items')
-
+    static removeUIBubbletea(el) {
+      if (el.classList.contains('removeBubbletea')) {
+        el.parentElement.parentElement.remove()
+      }
     }
   }
 
   $(".cartBtn").click(() => {
-    $("#cart-items").text('')
+    $("#cart-items").text("");
     cartUI.displayCartItems();
   });
 
-
-
-
+  $("#cart-items").click(e => {
+    cartUI.removeUIBubbletea(e.target);
+    // console.log(e.target.parentElement.nextElementSibling.textContent);
+    StoreBubbletea.removeBubbletea(
+      e.target.parentElement.nextElementSibling.textContent
+    );
+  })
 });
