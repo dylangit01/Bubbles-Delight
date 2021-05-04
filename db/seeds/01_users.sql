1ac57ca54e87f3661ea4044ce3e15957e1f4987a
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-04 09:23:49 -0400

    update seed for user. update spacing for navbar displaying logged in user name

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO users (name, email, password, phone_number)
 VALUES
-('Alice', 'alice@gmail.com', 'password', 4161231234),
+('Bubbletea Lover', 'bubbletealover@gmail.com', 'password', 4161231234),
 ('Kira', 'kira@gmail.com', 'password', 4161235678);
 
 INSERT INTO users (name, email, password, phone_number, is_admin)
