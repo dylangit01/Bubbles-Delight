13fbdea38c3b353549d4f44d5ab3084fb8010e8e
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-04 21:45:02 -0400

    change eta of in-progress to value instead of placeholder

--- public/scripts/restaurantActions.js
+++ public/scripts/restaurantActions.js
@@ -1,24 +1,22 @@
 
 
+// wrap each row in a form
+// event listener for the form on submit
+// play with this
+// For the select options
+// https://learn.jquery.com/using-jquery-core/faq/how-do-i-get-the-text-value-of-a-selected-option/
+// https://stackoverflow.com/questions/14460421/get-the-contents-of-a-table-row-with-a-button-click
 
+const restaurantSubmitHandler = function() {
+  const $row = $(this).closest("tr");
+  const $orderId = $row.find('.order-id').text();
+  const $eta = $row.find('input').val();
+  console.log($orderId, $eta);
 
-
+};
 
 
 $(document).ready(function() {
-  console.log("working");
-  // $('#submitOrders').click(e => {
-  //   e.preventDefault();
-  //   // jQuery cannot handle deep array, so have to send string
-  //   const order = localStorage.getItem("bubbletea");
-  //   console.log(order);
-
-  //   $.ajax({
-  //     type: "POST",
-  //     url: "/orders",
-  //     data: order,
-  //     contentType: "application/json"   // This is to prevent have FormData
-  //   });
-
-  // });
+  // Event listener on all the order status update buttons
+  $('.order-status-update').on('click', restaurantSubmitHandler);
 });
