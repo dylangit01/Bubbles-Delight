3198c5baf10c22e47030199c328cf06ee32266c9
Ruby-Zhuang <ruby.zhuang@hotmail.com>
2021-05-06 23:57:39 -0400

    remove dead code

--- public/scripts/cartCounter.js
+++ public/scripts/cartCounter.js
@@ -1,17 +1,4 @@
 $(document).ready(function() {
-  //  const $row = $(this).closest("tr"); // Find closest row <tr>
-  //  const orderID = $row.find(".order-id").text(); // Get descendent with order-id class
-  //  const eta = $row.find("input").val(); // Get descendent input element and it's value
-  //  const status = $row.find("select").val();
-
-  // $(".order-status-update").on("click", restaurantSubmitHandler);
-
-  // const findPickMeBtn = function () {
-  //   const $temp = $(this).closest("#addToCart");
-  //   console.log($temp);
-  // };
-
-  // $("#addToCart").on('click', findPickMeBtn);
 
   // Add chosen item and show cart Number indication:
 
