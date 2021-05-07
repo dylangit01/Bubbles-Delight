2f833d346b45c0a727a91fd32e64241b938135ac
Dylan <dcren2010@gmail.com>
2021-05-07 15:37:55 -0400

    update users.js

--- routes/users.js
+++ routes/users.js
@@ -5,19 +5,19 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   // Get all the users
   router.get("/", (req, res) => {
     const queryString = `SELECT * FROM users;`;
     db.query(queryString)
-      .then(data => {
+      .then((data) => {
         const users = data.rows;
         return res.json(users);
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json({ error: err.message });
       });
   });
