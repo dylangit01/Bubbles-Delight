0c814a8ab685ed7811f0d935f2f6ee24c1f0c393
Dylan <dcren2010@gmail.com>
2021-05-07 15:35:16 -0400

    update bubbletea route

--- routes/bubbleteas.js
+++ routes/bubbleteas.js
@@ -3,21 +3,21 @@
  * Since this file is loaded in server.js into /bubbleteas, these routes are mounted onto /bubbleteas
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 module.exports = (db) => {
   // Get all bubbleteas
   router.get("/", (req, res) => {
     const queryString = `SELECT * FROM bubbleteas;`;
     db.query(queryString)
-      .then(data => {
+      .then((data) => {
         const bubbleteas = data.rows;
         const user = req.user;
         const templateVars = { bubbleteas, user };
         return res.render("bubbleteas", templateVars);
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(500).json({ error: err.message });
       });
   });
